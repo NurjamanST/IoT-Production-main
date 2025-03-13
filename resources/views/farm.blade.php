d71808af02b497280a81369dfdb25abb56c81a97
NurjamanST <nurjamanst946@gmail.com>
2025-03-14 02:16:04 +0700

    Update Farm

--- resources/views/farm.blade.php
+++ resources/views/farm.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Smart Farm</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <style>
         html {
@@ -22,7 +23,7 @@
     <x-navbar>
         <img src="/image/Label/SmartFarm-Label.png" alt="SMART HOME Starter Kit" class="img-fluid position-absolute top-10 end-0" style="height: 50px; margin-right: 10px;">
     </x-navbar>
-
+    {{-- section for Title Page --}}
     <div class="container-fluid mt-4 pt-5 px-0"> 
         <div class="position-relative bg-dark text-white p-3 text-center">
             <a href="/" class="btn btn-light position-absolute start-0" style="top: 13px; margin-left: 10px;">Kembali</a>
@@ -31,11 +32,11 @@
     </div>
 
 
-
+    {{-- Section for the farm page --}}
     <div class="row mt-4 text-center justify-content-center">
         <!-- Update at for Kelembaban -->
-        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="border p-3 rounded shadow align-beetween">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 mx-5">
+            <div class="border p-3 rounded shadow">
                 <div class="">
                     <h5>07:00 WIB Selasa 12/03/2025</h5>
                 </div>
@@ -49,18 +50,18 @@
         </div>
 
         <!-- Kelembaban -->
-        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 mx-5">
             <div class="border p-3 rounded shadow">
                 <h5>KELEMBABAN</h5>
                 <img src="image/Icons/ph.png" alt="ph" class="img-fluid">
                 <div class="mt-2">
-                    <button class="btn btn-success w-100" >80 %</button>
+                    <button class="btn btn-primary w-100" >80 %</button>
                 </div>
             </div>
         </div>
 
         <!-- Kondisi POMPA -->
-        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 mx-5">
             <div class="border p-3 rounded shadow">
                 <h5>KONDISI POMPA</h5>
                 <img src="image/Icons/pompa.png" alt="Kipas" class="img-fluid">
@@ -71,24 +72,95 @@
         </div>
     </div>
 
-    <!-- Keterangan Sistem -->
-    <div class="mt-4 d-flex flex-column flex-md-row justify-content-between align-items-center bg-primary text-white p-3 full-width">
-        <span class="fw-bold">KETERANGAN SISTEM</span>
-        <div class="bg-white text-dark px-3 py-2 rounded text-left">
-            Sistem ini menggunakan sensor SOIL MOISTURE , untuk mendeteksi kadar kelembaban tanah. Jika kelembaban  tanah berada dibawah ambang batas, maka pompa Air akan otomatis menyala untuk menyiram tanaman.
+    {{-- Terakhir Pompa nyala --}}
+    <div class="container text-left my-3 p-3">
+        <div class="row">
+          <div class="col-6 col-md-8 px-3 d-flex align-items-center">
+            <h5>Terakhir Menyala Pada : 07:01 WIB Selasa 12/03/2025</h5>
+          </div>
+          <div class="col-6 col-md-4 px-3">
+            <button class="btn btn-success text-white px-3 rounded">KLIK UNTUK SIRAM SEKARANG</button>
+          </div>
         </div>
-        <span class="fw-bold">
-            Status Kelembaban Tanah :<span class="text-warning">80 %</span>
-            Status POMPA <span class="text-warning">MATI</span>
-        </span>
     </div>
 
+    <!-- Keterangan Sistem -->
+    <div class="d-flex flex-column flex-md-row justify-content-between align-items-center bg-primary text-white p-3 full-width">
+        <div class="col-12 col-md-auto mx-2">
+            <span class="fw-bold">KETERANGAN SISTEM</span>
+        </div>
+        <div class="col-12 col-md-8 bg-white text-dark px-3 py-2 rounded text-left">
+            Sistem ini menggunakan sensor SOIL MOISTURE , untuk mendeteksi kadar kelembaban tanah.
+            Jika kelembaban  tanah berada dibawah ambang batas, maka pompa Air akan otomatis menyala untuk menyiram tanaman.
+        </div>
+        <div class="col-12 col-md-auto mx-2">
+            <span class="fw-bold">
+                Status Kelembaban Tanah : <span class="text-warning">80 %</span><br>
+                Status POMPA : <span class="text-warning">MATI</span>
+            </span>
+        </div>
+    </div>
+
+    {{-- Tabel Riwayat Data - Smart Farm --}}
+    <div class="container">
+        <div class="row justify-content-center my-4">
+            <div class="col-12 col-md-10">
+                <h4>Riwayat Data - SMART FARM</h4>
+            </div>
+            <div class="col-12 col-md-auto">
+                <button class="btn btn-success text-white">Unduh File Excel</button>
+            </div>
+        </div>
+        <div class="row justify-content-center my-4">
+            <table id="example" class="display text-center" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Waktu</th>
+                        <th>Kelembaban</th>
+                        <th>Kondisi Pompa</th>
+                        <th>Terkakhir Menyala</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>80 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>91 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>102 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>113 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
     
     <x-footer></x-footer>
-    
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
     <script>
-                
+        new DataTable('#example');
         function toggleDevice(button, textOn, textOff) {
             if (button.classList.contains("btn-success")) {
                 button.classList.remove("btn-success");
--- storage/framework/views/c87ef195797f662e3b07407b3f5165a9.php
+++ storage/framework/views/c87ef195797f662e3b07407b3f5165a9.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Smart Farm</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo e(asset('assets/css/style.css')); ?>">
     <style>
         html {
@@ -40,7 +41,7 @@
 <?php $component = $__componentOriginalb9eddf53444261b5c229e9d8b9f1298e; ?>
 <?php unset($__componentOriginalb9eddf53444261b5c229e9d8b9f1298e); ?>
 <?php endif; ?>
-
+    
     <div class="container-fluid mt-4 pt-5 px-0"> 
         <div class="position-relative bg-dark text-white p-3 text-center">
             <a href="/" class="btn btn-light position-absolute start-0" style="top: 13px; margin-left: 10px;">Kembali</a>
@@ -49,11 +50,11 @@
     </div>
 
 
-
+    
     <div class="row mt-4 text-center justify-content-center">
         <!-- Update at for Kelembaban -->
-        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-            <div class="border p-3 rounded shadow align-beetween">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 mx-5">
+            <div class="border p-3 rounded shadow">
                 <div class="">
                     <h5>07:00 WIB Selasa 12/03/2025</h5>
                 </div>
@@ -67,18 +68,18 @@
         </div>
 
         <!-- Kelembaban -->
-        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 mx-5">
             <div class="border p-3 rounded shadow">
                 <h5>KELEMBABAN</h5>
                 <img src="image/Icons/ph.png" alt="ph" class="img-fluid">
                 <div class="mt-2">
-                    <button class="btn btn-success w-100" >80 %</button>
+                    <button class="btn btn-primary w-100" >80 %</button>
                 </div>
             </div>
         </div>
 
         <!-- Kondisi POMPA -->
-        <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-2 mx-5">
             <div class="border p-3 rounded shadow">
                 <h5>KONDISI POMPA</h5>
                 <img src="image/Icons/pompa.png" alt="Kipas" class="img-fluid">
@@ -89,19 +90,89 @@
         </div>
     </div>
 
-    <!-- Keterangan Sistem -->
-    <div class="mt-4 d-flex flex-column flex-md-row justify-content-between align-items-center bg-primary text-white p-3 full-width">
-        <span class="fw-bold">KETERANGAN SISTEM</span>
-        <div class="bg-white text-dark px-3 py-2 rounded text-left">
-            Sistem ini menggunakan sensor SOIL MOISTURE , untuk mendeteksi kadar kelembaban tanah. Jika kelembaban  tanah berada dibawah ambang batas, maka pompa Air akan otomatis menyala untuk menyiram tanaman.
+    
+    <div class="container text-left my-3 p-3">
+        <div class="row">
+          <div class="col-6 col-md-8 px-3 d-flex align-items-center">
+            <h5>Terakhir Menyala Pada : 07:01 WIB Selasa 12/03/2025</h5>
+          </div>
+          <div class="col-6 col-md-4 px-3">
+            <button class="btn btn-success text-white px-3 rounded">KLIK UNTUK SIRAM SEKARANG</button>
+          </div>
         </div>
-        <span class="fw-bold">
-            Status Kelembaban Tanah :<span class="text-warning">80 %</span>
-            Status POMPA <span class="text-warning">MATI</span>
-        </span>
+    </div>
+
+    <!-- Keterangan Sistem -->
+    <div class="d-flex flex-column flex-md-row justify-content-between align-items-center bg-primary text-white p-3 full-width">
+        <div class="col-12 col-md-auto mx-2">
+            <span class="fw-bold">KETERANGAN SISTEM</span>
+        </div>
+        <div class="col-12 col-md-8 bg-white text-dark px-3 py-2 rounded text-left">
+            Sistem ini menggunakan sensor SOIL MOISTURE , untuk mendeteksi kadar kelembaban tanah.
+            Jika kelembaban  tanah berada dibawah ambang batas, maka pompa Air akan otomatis menyala untuk menyiram tanaman.
+        </div>
+        <div class="col-12 col-md-auto mx-2">
+            <span class="fw-bold">
+                Status Kelembaban Tanah : <span class="text-warning">80 %</span><br>
+                Status POMPA : <span class="text-warning">MATI</span>
+            </span>
+        </div>
     </div>
 
     
+    <div class="container">
+        <div class="row justify-content-center my-4">
+            <div class="col-12 col-md-10">
+                <h4>Riwayat Data - SMART FARM</h4>
+            </div>
+            <div class="col-12 col-md-auto">
+                <button class="btn btn-success text-white">Unduh File Excel</button>
+            </div>
+        </div>
+        <div class="row justify-content-center my-4">
+            <table id="example" class="display text-center" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Waktu</th>
+                        <th>Kelembaban</th>
+                        <th>Kondisi Pompa</th>
+                        <th>Terkakhir Menyala</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>80 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>91 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>102 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                        <td>113 %</td>
+                        <td>Mati</td>
+                        <td>07:00 WIB Selasa, 12/03/2025</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
     
     <?php if (isset($component)) { $__componentOriginal8a8716efb3c62a45938aca52e78e0322 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginal8a8716efb3c62a45938aca52e78e0322 = $attributes; } ?>
@@ -122,9 +193,10 @@
 <?php $component = $__componentOriginal8a8716efb3c62a45938aca52e78e0322; ?>
 <?php unset($__componentOriginal8a8716efb3c62a45938aca52e78e0322); ?>
 <?php endif; ?>
-    
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
     <script>
-                
+        new DataTable('#example');
         function toggleDevice(button, textOn, textOff) {
             if (button.classList.contains("btn-success")) {
                 button.classList.remove("btn-success");
