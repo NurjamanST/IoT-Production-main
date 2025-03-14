7d317fbb6178f509a61c8e2198cf6439d5481b0d
NurjamanST <nurjamanst946@gmail.com>
2025-03-14 16:16:40 +0700

    Revert "jpg to png"
    This reverts commit 4f45d6e60016e410b4def7f107e76045b1df3570.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -174,7 +174,11 @@ html {
             <!-- Picture (10%) -->
             <div style="flex: 0 0 17%; height: 100%;">
               <div class="position-relative w-100 h-100">
+<<<<<<< HEAD
                 <img src="{{ asset('image/Produk/home2.jpg') }}" class="img-fluid rounded border w-100 h-100 object-fit-cover" alt="Thumbnail Foto">
+=======
+                <img src="{{ url('image/Produk/smarthome-starterkit.jpg') }}" class="img-fluid rounded border w-100 h-100 object-fit-cover" alt="Thumbnail Foto">
+>>>>>>> parent of d0c06f1 (test)
                 <div class="position-absolute top-50 start-50 translate-middle text-center fw-bold text-black" style="width: 100%; font-size: 10px;">
                   Thumbnail Products <br> (Picture)
                 </div>
@@ -201,7 +205,11 @@ html {
               </div>
               <div class="d-flex justify-content-between gap-2">
                 <a href="#" class="btn btn-primary btn-sm rounded">Selengkapnya <i class="bi bi-arrow-right"></i></a>
+<<<<<<< HEAD
                 <a href="{{ ('house') }}" class="btn btn-sm rounded" style="background-color: #0d2b57; color: white;">Demo Web <i class="bi bi-arrow-right"></i></a>
+=======
+                <a href="/house" class="btn btn-sm rounded" style="background-color: #0d2b57; color: white;">Demo Web <i class="bi bi-arrow-right"></i></a>
+>>>>>>> parent of d0c06f1 (test)
               </div>
             </div>
           </div>
@@ -252,7 +260,11 @@ html {
             <!-- Picture (10%) -->
             <div style="flex: 0 0 17%; height: 100%;">
               <div class="position-relative w-100 h-100">
+<<<<<<< HEAD
                 <img src="{{ asset('image/Produk/presence.jpg') }}" class="img-fluid rounded border w-100 h-100 object-fit-cover" alt="Thumbnail Foto">
+=======
+                <img src="{{ url('image/Produk/rfid-starterkit.jpg') }}" class="img-fluid rounded border w-100 h-100 object-fit-cover" alt="Thumbnail Foto">
+>>>>>>> parent of d0c06f1 (test)
                 <div class="position-absolute top-50 start-50 translate-middle text-center fw-bold text-black" style="width: 100%; font-size: 10px;">
                   Thumbnail Products <br> (Picture)
                 </div>
@@ -262,7 +274,11 @@ html {
             <!-- Video (20%) -->
             <div style="flex: 0 0 30%; height: 100%;">
               <div class="position-relative w-100 h-100">
+<<<<<<< HEAD
                 <img src="{{ asset('image/Produk/presence2.jpg') }}" class="img-fluid rounded border w-100 h-100 object-fit-cover" alt="Thumbnail Video">
+=======
+                <img src="{{ url('image/Produk/rfid-trainingkit.jpg') }}" class="img-fluid rounded border w-100 h-100 object-fit-cover" alt="Thumbnail Video">
+>>>>>>> parent of d0c06f1 (test)
                 <div class="position-absolute top-50 start-50 translate-middle text-center fw-bold text-black" style="width: 100%; font-size: 10px;">
                   Thumbnail Products <br> (Video)
                 </div>
